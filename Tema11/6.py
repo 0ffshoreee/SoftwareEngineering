0ab45c02ab40c636a95e99184a093a09987caea0
0ffshoreee <zelentsovkirill50@gmail.com>
2024-11-22 03:56:44 +0500

    Выгрузка самостоятельной и лабораторной работ

---
+++ Tema11/6.py
@@ -0,0 +1,11 @@
+def fib(n):
+    a, b = 1, 1
+    for num in range(n):
+        yield a
+        a, b = b, a + b
+
+# Генерируем числа Фибоначчи и выводим 200-е число
+n = 300
+fibonacci_numbers = list(fib(n))
+
+print(f"{n}-е число Фибоначчи: {fibonacci_numbers[-1]}")
